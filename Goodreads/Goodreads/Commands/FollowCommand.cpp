e86679d21b158a193c092660c733742038854ed3
Pavel Stoyanov <114023028+velstoy@users.noreply.github.com>
2026-06-15 03:23:03 +0300

    Implemented Follow

---
+++ Goodreads/Goodreads/Commands/FollowCommand.cpp
@@ -0,0 +1,31 @@
+#include "FollowCommand.h"
+#include <iostream>
+#include <format>
+#include "CommandUtils.h"
+#include "../Registries/UserRegistry.h"
+#include "../Exceptions/EntityNotFoundException.h"
+#include "../Exceptions/InvalidArgumentException.h"
+
+FollowCommand::FollowCommand(Session& session, const std::string& target)
+	: session(session), target(target) {}
+
+void FollowCommand::execute()
+{
+	auto me = CommandUtils::requireLogin(session);
+	if (target == me->getUsername())
+		throw InvalidArgumentException("You cannot follow yourself!");
+
+	auto user = UserRegistry::getInstance().find(target);
+	if (!user)
+		throw EntityNotFoundException("User", target);
+	if (user->isFollowedBy(me->getUsername()))
+		throw InvalidArgumentException("You already follow this user!");
+
+	user->addFollower(me);
+	user->receiveMessage(std::make_unique<Message>(
+		me, false, std::format("{} started following you.", me->getUsername()), MessageType::FriendRequest));
+
+	std::cout << "You are now following " << target << ".\n";
+}
+void FollowCommand::undo() {}
+bool FollowCommand::canUndo() const { return false; }
---
+++ Goodreads/Goodreads/Commands/FollowCommand.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <string>
+#include "ICommand.h"
+#include "../Core/Session.h"
+
+class FollowCommand : public ICommand
+{
+private:
+	Session& session;
+	std::string target;
+public:
+	FollowCommand(Session& session, const std::string& target);
+	void execute() override;
+	void undo() override;
+	bool canUndo() const override;
+};
