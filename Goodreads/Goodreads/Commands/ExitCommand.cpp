9601b557e479e4efc2265e33ec83c1d0e2a4743f
Pavel Stoyanov <114023028+velstoy@users.noreply.github.com>
2026-06-15 03:11:01 +0300

    Completed exit

---
+++ Goodreads/Goodreads/Commands/ExitCommand.cpp
@@ -0,0 +1,15 @@
+#include "ExitCommand.h"
+#include <iostream>
+#include "../Persistence/FileManager.h"
+
+ExitCommand::ExitCommand(bool& running, const std::string& savePath)
+	: running(running), savePath(savePath) {}
+
+void ExitCommand::execute()
+{
+	FileManager::save(savePath);
+	std::cout << "Data saved. Goodbye!\n";
+	running = false;
+}
+void ExitCommand::undo() {}
+bool ExitCommand::canUndo() const { return false; }
---
+++ Goodreads/Goodreads/Commands/ExitCommand.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+#include "ICommand.h"
+
+class ExitCommand : public ICommand
+{
+private:
+	bool& running;
+	std::string savePath;
+public:
+	ExitCommand(bool& running, const std::string& savePath);
+	void execute() override;
+	void undo() override;
+	bool canUndo() const override;
+};
