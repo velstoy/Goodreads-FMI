96deb9cfd88fde4241bc7d5a2b5bd135b4b7b538
Pavel Stoyanov <114023028+velstoy@users.noreply.github.com>
2026-06-18 19:57:00 +0300

    removed old txt

--- Goodreads/Goodreads/Commands/ICommand.h
+++ Goodreads/Goodreads/Commands/ICommand.h
@@ -1,7 +1,5 @@
 #pragma once
 
-// Command pattern: each user action is its own command object, so the engine
-// can validate and run them all the same way.
 class ICommand
 {
 public:
--- Goodreads/Goodreads/Factories/CommandFactory.h
+++ Goodreads/Goodreads/Factories/CommandFactory.h
@@ -5,9 +5,6 @@
 #include "../Commands/ICommand.h"
 #include "../Core/Session.h"
 
-// Factory pattern: builds the right command object from the words of a command
-// line. Unknown commands and wrong argument counts are caught here, so each
-// command can assume its input is already valid.
 class CommandFactory
 {
 public:
